e3c650cd9c87f2c10593bf5dbe147666eeb3ebc7
mix3 <himachocost333@hotmail.co.jp>
2014-10-31 23:12:15 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+SQL::Translator::Producer::PlantUML - It's new $module
+
+# SYNOPSIS
+
+    use SQL::Translator::Producer::PlantUML;
+
+# DESCRIPTION
+
+SQL::Translator::Producer::PlantUML is ...
+
+# LICENSE
+
+Copyright (C) mix3.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mix3 <himachocost333@hotmail.co.jp>
---
+++ lib/SQL/Translator/Producer/PlantUML.pm
@@ -0,0 +1,39 @@
+package SQL::Translator::Producer::PlantUML;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SQL::Translator::Producer::PlantUML - It's new $module
+
+=head1 SYNOPSIS
+
+    use SQL::Translator::Producer::PlantUML;
+
+=head1 DESCRIPTION
+
+SQL::Translator::Producer::PlantUML is ...
+
+=head1 LICENSE
+
+Copyright (C) mix3.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mix3 E<lt>himachocost333@hotmail.co.jpE<gt>
+
+=cut
+
